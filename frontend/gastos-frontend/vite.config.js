0b825daea561f5a170bd4e484131c5d4b4df288b
GuillermoNavarro <guillenava@gmail.com>
2026-06-28 13:15:21 +0000

    añadir configuracion PWA

--- frontend/gastos-frontend/vite.config.js
+++ frontend/gastos-frontend/vite.config.js
@@ -3,12 +3,36 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      manifest: {
+        name: 'Gestion de Cuentas',
+        short_name: 'Cuentas',
+        description: 'App para el control de cuentas del hogar',
+        theme_color: '#5754ff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
